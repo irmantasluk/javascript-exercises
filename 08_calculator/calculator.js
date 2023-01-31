3ef436629251d3b7bc95b2506806f90dcae137b9
irmantasluk <9593928+irmantasluk@users.noreply.github.com>
2023-01-30 21:10:12 -0500

    complete calculator excersize

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, currVal) => acc + currVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, currVal) => acc * currVal, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+	if(num == 0 || num == 1){
+    return 1
+  }
+  // while(num > 1){
+  //   num--;
+  //   result *= num;
+  // }
+  // return result;
+
+  for(i = num; i > 1; i--){
+    result *= (i - 1);
+  }
+  return result;
 };
 
 // Do not edit below this line
