e8ddb6a466cfa16b740251d1382ccefb95a9a81c
irmantasluk <9593928+irmantasluk@users.noreply.github.com>
2023-01-31 20:32:42 -0500

    updated to cleaner code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,19 +1,28 @@
 const palindromes = function (string) {
-    let strCleaned= string
-        .toLowerCase()
-        .replace( /[^a-zA-Z ]/g, '')
-        .replace( /\s\s+/g, ' ' )
-        .split(' ');
+    // let strCleaned= string
+    //     .toLowerCase()
+    //     .replace( /[^a-zA-Z ]/g, '')
+    //     .replace( /\s\s+/g, ' ' )
+    //     .split(' ');
 
-    const strRegular = strCleaned.join('');
-    let strReversed ="";
+    // const strRegular = strCleaned.join('');
+    // let strReversed ="";
 
-    for(let i = strRegular.length -1; i >= 0; i--){
-        strReversed += strRegular[i];
-    }
+    // for(let i = strRegular.length -1; i >= 0; i--){
+    //     strReversed += strRegular[i];
+    // }
 
-    return strRegular === strReversed ? true : false;
+    // return strRegular === strReversed ? true : false;
+
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+
+    return processedString
+        .split('')
+        .reverse()
+        .join('') == processedString;
 };
+
+palindromes("asde ared , sfdsg.")
 
 // Do not edit below this line
 module.exports = palindromes;
