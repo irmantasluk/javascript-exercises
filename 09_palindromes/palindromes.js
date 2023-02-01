64e171af31866d590b639a04bd3d55b36adbb162
irmantasluk <9593928+irmantasluk@users.noreply.github.com>
2023-01-31 20:02:35 -0500

    create palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let strCleaned= string
+        .toLowerCase()
+        .replace( /[^a-zA-Z ]/g, '')
+        .replace( /\s\s+/g, ' ' )
+        .split(' ');
 
+    const strRegular = strCleaned.join('');
+    let strReversed ="";
+
+    for(let i = strRegular.length -1; i >= 0; i--){
+        strReversed += strRegular[i];
+    }
+
+    return strRegular === strReversed ? true : false;
 };
 
 // Do not edit below this line
