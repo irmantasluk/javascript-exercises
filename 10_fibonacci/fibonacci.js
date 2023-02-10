68eb7f602ca3b358f10fe0c0587c552a3d09fde9
irmantasluk <9593928+irmantasluk@users.noreply.github.com>
2023-02-10 08:11:08 -0500

    complete excersize

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if(n === 0){
+        return 0;
+    } else if(n === 1){
+        return 1;
+    } else if(n < 0){
+        return "OOPS";
+    } else {
+        let a = 0, b = 1, c;
+        for(let i = 2; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};      
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // let titles = [];
+    // books.forEach((book) => { titles.push(book.title) });
+    // return titles;
+    return books.map((book) => book.title);
 };
+ getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
